b8a342bb65b1655fc10ad409f011b28e3f31ffea
Elijah Goh <iamchoonggg@gmail.com>
2025-03-30 00:10:29 +0800

    style: Fix indentation in Navbar.svelte component

--- src/lib/components/CommandCentre/Navbar.svelte
+++ src/lib/components/CommandCentre/Navbar.svelte
@@ -13,7 +13,7 @@
       </div>
       <div class="text-gray-900 px-3 py-2 text-sm font-medium">{user.name}</div>
       <div class="-mr-2 flex items-center md:hidden">
-        <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+      <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
           <span class="sr-only">Open main menu</span>
           <!-- Hamburger icon -->
           <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
