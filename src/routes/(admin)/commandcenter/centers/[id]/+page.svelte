143feade868e9f6442c8ef18308f7b987f1d979d
Elijah Goh <iamchoonggg@gmail.com>
2025-03-31 22:24:37 +0800

    Add the center name

---
+++ src/routes/(admin)/commandcenter/centers/[id]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  let { data } = $props();
+  $: center = data.center;
+</script>
+
+<div class="container mx-auto p-4">
+  <h1 class="text-2xl font-bold mb-4">Center: {center.name}</h1>
+</div>
